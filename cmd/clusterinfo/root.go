5ec3148314c24a9d7127e55f6ba3b50b7fd24321
jeremy woo <jeremy>
2022-09-14 22:46:02 +1200

    update message

--- cmd/clusterinfo/root.go
+++ cmd/clusterinfo/root.go
@@ -9,16 +9,26 @@ import (
 )
 
 // Example of use case
-// kubesausage.exe --type=cluster --version=10.20.30
+// kubesausage.exe version
+// kubesausage.exe evict
 
 func Execute() {
 
 	var informationType string
 
 	var versionCmd = &cobra.Command{
-		Use:   "version",
-		Short: "Get version info",
+		Use:   `version`,
+		Short: `Get version info`,
 		Long:  `Get version info`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("version 1.0")
+		},
+	}
+
+	var evictCmd = &cobra.Command{
+		Use:   `evict`,
+		Short: `Evit a pod from a namespace`,
+		Long:  `Evit a pod from a namespace`,
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("version 1.0")
 		},
@@ -37,10 +47,11 @@ func Execute() {
 		},
 	}
 
-	rootCmd.Flags().StringVarP(&informationType, "type", "t", "inplace", "Upgrade type")
-	rootCmd.MarkFlagRequired("type")
+	//rootCmd.Flags().StringVarP(&informationType, "type", "t", "inplace", "Upgrade type")
+	//rootCmd.MarkFlagRequired("type")
 
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.AddCommand(evictCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
